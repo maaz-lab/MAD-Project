6faffa0705062b0ff816a60e6ff49ba7ee1d0ef0
Muhammad Haris <369haris@gmail.com>
2024-01-14 19:27:26 +0500

    App Styles added

---
+++ Application/Code/news_aggregator/lib/resources/constants/style.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  // Padding
+  static double padding = 16;
+
+  // Space
+  static Widget space = SizedBox(
+    height: padding,
+  );
+
+  static Widget space10 = const SizedBox(
+    height: 10,
+  );
+
+  static Widget space20 = const SizedBox(
+    height: 20,
+  );
+
+  static Widget space40 = const SizedBox(
+    height: 40,
+  );
+
+  static Widget gap = SizedBox(
+    width: padding,
+  );
+
+  static Widget gap10 = const SizedBox(
+    width: 10,
+  );
+
+  // Text Styles
+  static TextStyle tag =
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.w400);
+
+  static TextStyle heading =
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.w400);
+
+  static TextStyle title =
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w400);
+
+  static TextStyle body =
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
+}
