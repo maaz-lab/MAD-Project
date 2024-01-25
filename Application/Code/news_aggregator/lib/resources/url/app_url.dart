e503e988cca325abb0de9362baefd1250a90a314
Muhammad Haris <369haris@gmail.com>
2024-01-26 01:02:00 +0500

    Api EndPoints added

---
+++ Application/Code/news_aggregator/lib/resources/url/app_url.dart
@@ -0,0 +1,9 @@
+class AppUrl {
+  static String baseUrl = "http://10.0.2.2:3000";
+
+  static String expressEndPoint = "$baseUrl/express-news";
+
+  static String geoEndPoint = "$baseUrl/geo-news";
+
+  static String bolEndPoint = "$baseUrl/bol-news";
+}
